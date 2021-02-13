97690b22fb14150bf6295297ad86c28460949a72
honda-best <74924378+honda-best@users.noreply.github.com>
2021-02-13 23:07:40 +0300

    3

--- js/tikTakBoomTasks.js
+++ js/tikTakBoomTasks.js
@@ -1,8 +1,8 @@
 const tasks = `
 
     [
-        {
-            "question": "2 * 2 = ",
+          {
+            "question": "Сколько ног у жирафа?",
             "answer1": { "result": true, "value": "4" },
             "answer2": { "result": false, "value": "3" }
         },
@@ -12,14 +12,94 @@ const tasks = `
             "answer2": { "result": true, "value": "Гена" }
         },
         {
-            "question": "5 + 5",
-            "answer1": { "result": false, "value": "1" },
-            "answer2": { "result": true, "value": "10" }
+            "question": "Чем питается тигр?",
+            "answer1": { "result": false, "value": "Пингвины" },
+            "answer2": { "result": true, "value": "Косули" }
         },
         {
-            "question": "Столица Италии?",
-            "answer1": { "result": false, "value": "Ватикан" },
-            "answer2": { "result": true, "value": "Рим" }
+            "question": "На каком полюсе живут белые медведи?",
+            "answer1": { "result": false, "value": "Южный" },
+            "answer2": { "result": true, "value": "Северный" }
+        },
+        {
+            "question": "Какие животные были в фильме Полосатый рейс?",
+            "answer1": { "result": false, "value": "Верблюды" },
+            "answer2": { "result": true, "value": "Тигры" }
+        },
+        {
+            "question": "Где живут пираньи?",
+            "answer1": { "result": true, "value": "В реках" },
+            "answer2": { "result": false, "value": "В океанах" }
+        },
+        {
+            "question": "Сколько дельфин может жить без воздуха?",
+            "answer1": { "result": true, "value": "2 часа" },
+            "answer2": { "result": false, "value": "2 дня" }
+        },
+        {
+            "question": "Чем питается кит?",
+            "answer1": { "result": true, "value": "Планктоном" },
+            "answer2": { "result": false, "value": "Дельфинами" }
+        },
+        {
+            "question": "Какого окраса гималайский медведь?",
+            "answer1": { "result": false, "value": "Белый" },
+            "answer2": { "result": true, "value": "Бурый" }
+        },
+        {
+            "question": "Сколько ног у краба?",
+            "answer1": { "result": false, "value": "4" },
+            "answer2": { "result": true, "value": "6" }
+        },
+        {
+            "question": "Где живут осьминоги?",
+            "answer1": { "result": false, "value": "В реках" },
+            "answer2": { "result": true, "value": "В океанах" }
+        },
+        {
+            "question": "Чем питаются собаки?",
+            "answer1": { "result": true, "value": "Планктоном" },
+            "answer2": { "result": false, "value": "Мясом" }
+        },
+        {
+            "question": "Сколько ног у слона?",
+            "answer1": { "result": true, "value": "4" },
+            "answer2": { "result": false, "value": "2" }
+        },
+        {
+            "question": "Что у слона длиннее хобота?",
+            "answer1": { "result": true, "value": "Сопли" },
+            "answer2": { "result": false, "value": "Хвост" }
+        },
+        {
+            "question": "Сколько языков знают приматы?",
+            "answer1": { "result": true, "value": "Один" },
+            "answer2": { "result": false, "value": "Ни одного" }
+        },
+        {
+            "question": "Где живут хорьки?",
+            "answer1": { "result": false, "value": "В реках" },
+            "answer2": { "result": true, "value": "В норах" }
+        },
+        {
+            "question": "Какого цвета зебра?",
+            "answer1": { "result": false, "value": "Белая в черную полоску" },
+            "answer2": { "result": true, "value": "Черная в белую полоску" }
+        },
+        {
+            "question": "Сколько рядов зубов у акул?",
+            "answer1": { "result": false, "value": "1" },
+            "answer2": { "result": true, "value": "3" }
+        }
+        {
+            "question": "Что делать при виде волка?",
+            "answer1": { "result": true, "value": "Снимать мокрые штаны и бить ими по морде" },
+            "answer2": { "result": false, "value": "Бежать" }
+        },
+        {
+            "question": "Сколько лет живут черепахи?",
+            "answer1": { "result": true, "value": "больше 100" },
+            "answer2": { "result": false, "value": "не больше 5" }
         }
     ]
 `;
