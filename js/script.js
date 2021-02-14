4d3744e6a2ca4cf64665cba299dea23000bd23a7
honda-best <honda-best@ya.ru>
2021-02-14 15:47:36 +0300

    design

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const startStopBtn = document.querySelector('#startstop');
+let gameRun = 1;
+
+function startStop() {
+	if (gameRun) {
+		tikTakBoom.run();
+    	startStopBtn.className = "btn btn-block btn-outline-danger";
+    	startStopBtn.textContent = "Закончить игру";
+    	gameRun = 0;
+	} else {
+		tikTakBoom.finish('lose');
+		startStopBtn.className = "btn btn-block btn-outline-primary";
+    	startStopBtn.textContent = "Начать игру";
+    	gameRun = 1;
+	}
+}
+
+startStopBtn.addEventListener('click', startStop);
+
+window.onload = function()
+{
+    tikTakBoom.init(
+        tasks,
+        document.getElementById('timerField'),
+        document.getElementById('gameStatusField'),
+        document.getElementById('questionField'),
+        document.getElementById('answer1'),
+        document.getElementById('answer2'),
+        startStopBtn,
+    )
+
+
+}
